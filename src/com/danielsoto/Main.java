6996f60463bc0ca2fe9aae9129deb0b2357a0400
Daniel Soto <dasg.1993@gmail.com>
2022-05-20 12:41:11 -0500

    [topic] - variable - data types

---
+++ src/com/danielsoto/Main.java
@@ -0,0 +1,29 @@
+package com.danielsoto;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Primitive data types
+        boolean isDone = false; // true / false 1 bit in 32 bit
+        // Numeric Types
+        // Character Value
+        char myChar = 'A';
+        // Integral value
+        // integer
+        byte myByte = 127; // 8 bit
+        short myShort = 32767; // 16 bit
+        int myInt = 2147483647; // 32 bit
+        long myLong = 9223372036854775807L; // 64 bit
+        // floating point
+        float myFloat = 3.14f; // 32 bit
+        double myDouble = 3.14; // 64 bit
+
+        // Non primitive data types
+        String myString = "Hello World";
+        // Array
+        int[] myArray = {1, 2, 3, 4, 5};
+
+	// write your code here
+        System.out.println("Hello World!");
+    }
+}
